c9e246fd21beffbc906493963eb48b357c00f85e
Chainamnaris, Sutthidol <87947943+ChainamnarisSutthidol@users.noreply.github.com>
2023-03-15 10:15:59 -0700

    Create bot_Sutthidol2.py

---
+++ bot_Sutthidol2.py
@@ -0,0 +1,13 @@
+def choice(round_score, my_score, opponent_score):
+  if my_score > 70 and opponent_score +40 < my_score:
+    if round_score < 15:
+      return True
+    elif round_core > 15:
+      return False
+  if opponent_score > my_score + 20:
+    if round_score < 30:
+      return True
+    if opponent_score > my_score:
+      if round_score < 20:
+        return True
+    
