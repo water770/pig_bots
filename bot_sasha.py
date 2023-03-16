cc000c53ffa6b3f5245c7c9571b89f6fff87f2e5
Vitryanyuk, Sasha <90406739+PusheenTheCat5@users.noreply.github.com>
2023-03-16 13:42:31 -0700

    Update bot_sasha.py

--- bot_sasha.py
+++ bot_sasha.py
@@ -1,13 +1,12 @@
 def choice(round_score, my_score, opponent_score):
-    return round_score <= 20
-  if opponent_score > my_score:
+    if opponent_score > my_score:
         if round_score < (opponent_score-my_score):
             return True
         if round_score > (opponent_score-my_score):
              return False
-if opponent_score < my_score:
-        if round_score <= 25:
+    if opponent_score < my_score:
+        if round_score <= 20:
              return False
-if opponent_score == my_score:
-        if round_score <= 27:
+    if opponent_score == my_score:
+        if round_score <= 25:
               return False
