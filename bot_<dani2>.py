037c4319d01895f68fc6e98dcab7d2acb9e79491
Chris Bradfield <cb@scribe.net>
2023-03-16 19:31:53 -0700

    updates

--- bot_<dani2>.py
+++ bot_<dani2>.py
@@ -14,8 +14,8 @@ def choice(round_score, my_score, opponent_score):
         return True
     else:
         return False
-  elif opponent_score > my_score and opponent_score > 75 and oppponent score < 80:
-    if round_score < 30 or score + my_score > 99:
+  elif opponent_score > my_score and opponent_score > 75 and opponent_score < 80:
+    if round_score < 30 or round_score + my_score > 99:
         return True
     else:
         return False
