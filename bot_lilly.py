6d65e3f510d297a3387d94f44e8c088f800d8c4a
Kelsay, Lily <105990732+Sprxxng@users.noreply.github.com>
2023-03-17 09:04:44 -0700

    Update bot_lilly.py

--- bot_lilly.py
+++ bot_lilly.py
@@ -2,7 +2,7 @@ def choice(round_score,my_score, opponent_score)
       num = 21
   ##if opponent_score - my_score >= 30:
            ##num = 26
-      if my_score[0] + round_score >=100:
+      if my_score + round_score >=100:
           return False
       if opponent_score > 85 and my_score + round_score<100 and round_score< 45:
           return True
