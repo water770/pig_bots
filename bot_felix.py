50c14ea5ece3344ba6cb8470ce25bc7a09ce4e61
Said, Felix <90422837+EpicFishy@users.noreply.github.com>
2023-03-17 18:21:21 -0700

    Create bot_felix.py

---
+++ bot_felix.py
@@ -0,0 +1,7 @@
+def choice(round_score, my_score, opponent_score):
+        z = (random.randint(1, 100))
+        z = z/100
+        if z > 0.13:
+            return True
+        else:
+            return False
