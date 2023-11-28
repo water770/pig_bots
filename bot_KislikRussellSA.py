f90c74fbbc29994055bbe01993a39845b93d4f47
Kislik, Russell <71467186+KislikRussellSA@users.noreply.github.com>
2023-11-28 13:43:26 -0800

    Create bot_KislikRussellSA.py

---
+++ bot_KislikRussellSA.py
@@ -0,0 +1,5 @@
+def choice(round_score, my_score, opponent_score):
+    if round_score > floor(20 + 10*opponent_score/100) or my_score + round_score >= 100:
+        return False
+    else:
+        return True
