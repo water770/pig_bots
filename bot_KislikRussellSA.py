8a92d465f91b514870161b029f5534863e3ec267
Kislik, Russell <71467186+KislikRussellSA@users.noreply.github.com>
2023-11-30 13:53:28 -0800

    Update bot_KislikRussellSA.py

--- bot_KislikRussellSA.py
+++ bot_KislikRussellSA.py
@@ -1,6 +1,5 @@
-from math import floor
 def choice(round_score, my_score, opponent_score):
-    if round_score > 20 + 10*opponent_score/100 or my_score + round_score >= 100:
+    if round_score > 21 + 3.*opponent_score/100 + 4.5*(my_score)/100 or my_score + round_score >= 100:
         return False
     else:
         return True
