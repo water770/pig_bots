38897b6f9a593daef827becb6980de8f11bc6e0f
Aguilar, Josh <71454396+AguilarJoshSA@users.noreply.github.com>
2023-12-06 10:42:00 -0800

    Create bot_jaguilar1.py

---
+++ bot_jaguilar1.py
@@ -0,0 +1,5 @@
+def choice(round_score, my_score, opponent_score):
+    # TODO: return True if the player wants to roll again
+    #       return False if the player wants to hold
+    # example (bad) strategy:
+    return round_score <= 12
