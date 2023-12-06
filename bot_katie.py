bf8a72594b1cf26d4c4ed60386de3759eca53e42
Shin, Katie <71454407+ShinKatieSA@users.noreply.github.com>
2023-12-06 08:54:10 -0800

    Update bot_katie.py

--- bot_katie.py
+++ bot_katie.py
@@ -1,5 +1,7 @@
 
 def choice(round_score, my_score, opponent_score):
+    if my_score == 0 and opponent_score == 0:
+        return round_score <= 40
     if round_score < 15:
         return True
     if opponent_score > 85 & opponent_score - my_score > 40:
