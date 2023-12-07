642143c936a6786effae2cc73cd3bf08b63a050c
Chris Bradfield <cb@scribe.net>
2023-12-06 22:38:00 -0800

    fix typos

--- bot_komatireddy3.py
+++ bot_komatireddy3.py
@@ -6,14 +6,14 @@ def choice(round_score, my_score, opponent_score):
             return True
           else:
             return False
-        if my_score < opponent_score:
+      if my_score < opponent_score:
           if round_score < 21 + (opponent_score - my_score) // 8:
             return True
           else:
             return False
-        else:
+      else:
             if round_score < 21:
               return True
             else:
               return False
-        return round_score <= 12
+      return round_score <= 12
